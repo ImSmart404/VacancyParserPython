45124de4da6f4211b79b025c1bbecb3f8bf03ad5
Suzuya Juzo <withfuckinglove@mail.ru>
2023-11-18 19:53:53 +0300

    some fixes

--- Controllers/MainController.py
+++ Controllers/MainController.py
@@ -11,6 +11,6 @@ def index():
     vacancy_title = request.form.get('vacancy_title')
     data_from_hh = HeadHunterParser.get_vacancies(vacancy_title, 1)
 
-    # Преобразование данных в формат JSON с использованием json.dumps и добавление параметра default
+    # Преобразование данных в формат JSON с использованием json.dumps и добавление параметра default для преобразования обьектов VacancyDTO в словари
     json_data = json.dumps(data_from_hh, default=lambda o: o.__dict__, ensure_ascii=False)
     return json_data, 200, {'Content-Type': 'application/json; charset=utf-8'}
--- Service/HhParser.py
+++ Service/HhParser.py
@@ -18,50 +18,10 @@ class HeadHunterParser:
         # Отправляем запрос с установленными параметрами.
         response = requests.get(HeadHunterParser.url, params=params)
 
-        # Проверьте, является ли код состояния ответа 200 (OK), прежде чем пытаться разобрать JSON.
+        # Успешный сценарий
         if response.status_code == 200:
             data_vacancy = response.json().get('items', [])
             return VacancyDTO.parse_vacancy_data(data_vacancy)  # Добавлен возврат результата
         else:
-            # Обработайте ошибку или вызовите исключение при необходимости.
+            # Обработка ошибок
             response.raise_for_status()
-
-    @staticmethod
-    def data_organize(data_vacancy) -> list[dict]:
-        """
-        Организация данных по вакансиям.
-        Возвращает сформированный список словарей.
-        """
-        vacancies = []
-        for vacancy in data_vacancy:
-            title = vacancy['name']
-            link = vacancy['alternate_url']
-            requirement = vacancy['snippet'].get('requirement', None)
-            salary_from = vacancy['salary'].get('from', None)
-            salary_to = vacancy['salary'].get('to', None)
-
-            # Устанавливаем значение зарплаты.
-            # Если есть обе границы - устанавливаем минимальное значение.
-            # В ином случае, устанавливаем то значение, которое существует.
-            if salary_from and salary_to:
-                salary = min(salary_from, salary_to)
-            else:
-                salary = salary_from or salary_to
-
-            # Устанавливаем значение требований.
-            # Если значение есть - приводим его к нижнему регистру.
-            # В ином случае, устанавливаем значение, что данных нет.
-            if requirement:
-                requirement = requirement.lower()
-            else:
-                requirement = 'Нет данных.'
-
-            # Добавляем сформированные данные в список.
-            vacancies.append({
-                'title': title,
-                'link': link,
-                'salary': salary,
-                'requirement': requirement
-            })
-
-        return vacancies
