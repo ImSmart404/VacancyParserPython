4b8c1b8c87d09b23489b9ce196223fbe4521dc47
Suzuya Juzo <withfuckinglove@mail.ru>
2023-11-18 19:25:00 +0300

    parse hh.ru as html

--- Controllers/MainController.py
+++ Controllers/MainController.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request
-from Service.HhParser import HhParser
+from Service.HhParser import HeadHunterParser
 
 main_controller_app = Blueprint('main_controller', __name__)
 
@@ -9,5 +9,5 @@ main_controller_app.url_prefix = '/main'
 @main_controller_app.route('/', methods=['POST'])
 def index():
     vacancy_title = request.form.get('vacancy_title')
-    data_from_hh = HhParser.get_data(vacancy_title)
+    data_from_hh = HeadHunterParser.get_vacancies(vacancy_title, 1)
     return f"Данные для вакансии '{vacancy_title}' из HH.ru: {data_from_hh}"
