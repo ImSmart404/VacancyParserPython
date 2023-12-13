50b4286492741c74902ecab624a37023e9a1c09e
Suzuya Juzo <withfuckinglove@mail.ru>
2023-12-13 12:41:15 +0300

    fix service + controller, add route for favourites in controller and add docker-compose

--- Controllers/MainController.py
+++ Controllers/MainController.py
@@ -1,6 +1,5 @@
-from flask import Blueprint, render_template, request, jsonify, json
-from Service.HhParser import HeadHunterParser
-from Service.SuperJopParser import SuperJobParser
+from flask import Blueprint, request, json
+from Service.Parser import Parser
 from Service.ParseRegionJson import get_city_id
 
 main_controller_app = Blueprint('main_controller', __name__)
@@ -8,25 +7,20 @@ main_controller_app = Blueprint('main_controller', __name__)
 main_controller_app.url_prefix = '/main'
 
 
-@main_controller_app.route('/hh', methods=['POST'])
+@main_controller_app.route('/service', methods=['POST'])
 def hh():
     vacancy_title = request.form.get('vacancy_title')
     service_name = request.form.get('service_name')
     city_name = request.form.get('city_name')
     city_id = get_city_id(service_name, city_name)
-    data_from_hh = HeadHunterParser.get_vacancies(vacancy_title, city_id)
+    if (service_name == "hh"):
+        data_from_service = Parser.get_vacancies_from_hh(vacancy_title, city_id)
+    if (service_name == "sj"):
+        data_from_service = Parser.get_vacancies_from_sj(vacancy_title, city_id)
     # Преобразование данных в формат JSON с использованием json.dumps и добавление параметра default для преобразования обьектов VacancyDTO в словари
-    json_data = json.dumps(data_from_hh, default=lambda o: o.__dict__, ensure_ascii=False)
+    json_data = json.dumps(data_from_service, default=lambda o: o.__dict__, ensure_ascii=False)
     return json_data, 200, {'Content-Type': 'application/json; charset=utf-8'}
+@main_controller_app.route('/favourites', methods=['POST'])
+def favourites():
+    vacancy_id = request.form.get('id')
 
-
-@main_controller_app.route('/sj', methods=['POST'])
-def sj():
-    vacancy_title = request.form.get('vacancy_title')
-    service_name = request.form.get('service_name')
-    city_name = request.form.get('city_name')
-    city_id = get_city_id(service_name, city_name)
-    data_from_sj = SuperJobParser.get_vacancies(vacancy_title, city_id)
-    # Преобразование данных в формат JSON с использованием json.dumps и добавление параметра default для преобразования обьектов VacancyDTO в словари
-    json_data = json.dumps(data_from_sj, default=lambda o: o.__dict__, ensure_ascii=False)
-    return json_data, 200, {'Content-Type': 'application/json; charset=utf-8'}
